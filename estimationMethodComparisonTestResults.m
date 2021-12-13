fbf1b4661c894024fb0fccfa46669a29a0487744
cbonn <bonnerc3@myumanitoba.ca>
2021-12-13 12:43:59 -0600

    Calculating Max Error
    When comparing estimation methods, the max error is also calculated in addition to mean error to get a measure of outliers.

--- estimationMethodComparisonTestResults.m
+++ estimationMethodComparisonTestResults.m
@@ -1,7 +1,7 @@
 % Testing the relationship between forward and backward transitions
 % matrices, as well as the relationship between backward transition
 % matrices when collisions are(not) allowed.
-function estimationMethodComparisonTestResults(pid)
+function test(pid)
 disp(num2str(pid));
 par = setup;
 load(fullfile(par.scratchDir, 'coordination'));
@@ -13,6 +13,10 @@ mean_error_prob = zeros(par.numTests, 1);
 mean_error_est = mean_error_prob;
 mean_error_trad = mean_error_prob;
 mean_error_new = mean_error_prob;
+max_error_prob = zeros(par.numTests, 1);
+max_error_est = mean_error_prob;
+max_error_trad = mean_error_prob;
+max_error_new = mean_error_prob;
 testValues = coordination.reserved.(['process', num2str(pid)]);
 for j=1:length(testValues)
     par.(par.testField) = testValues(j);
@@ -20,24 +24,44 @@ for j=1:length(testValues)
     for i=1:par.numTests
         
         casino = TM_test(par);
-
+        
+        %UnifOcc
         mean_error = abs(casino.TM-casino.TM0);
         mean_error = mean(mean_error, 1);
         mean_error_prob(i) = mean(mean_error);
+        
+        max_error = abs(casino.TM-casino.TM0);
+        max_error = max(max_error);
+        max_error_prob(i) = max(max_error);
         %disp(['Mean error in traditional method: ', num2str(mean_error_trad(i))]);
-
+        
+        %AddX
         mean_error = abs(casino.TM_est-casino.TM0);
         mean_error = mean(mean_error, 1);
         mean_error_est(i) = mean(mean_error);
+        
+        max_error = abs(casino.TM_est-casino.TM0);
+        max_error = max(max_error);
+        max_error_est(i) = max(max_error);
         %disp(['Mean error in estimation method: ', num2str(mean_error_est(i))]);
-
+        
+        %MLE
         mean_error = abs(casino.P_move-casino.TM0);
         mean_error = mean(mean_error, 1);
         mean_error_trad(i) = mean(mean_error);
         
+        max_error = abs(casino.P_move-casino.TM0);
+        max_error = max(max_error);
+        max_error_trad(i) = max(max_error);
+        
+        %ProbUpdate
         mean_error = abs(casino.TM_new-casino.TM0);
         mean_error = mean(mean_error, 1);
         mean_error_new(i) = mean(mean_error);
+        
+        max_error = abs(casino.TM_new-casino.TM0);
+        max_error = max(max_error);
+        max_error_new(i) = max(max_error);
         %disp(['Mean error in p_move method: ', num2str(mean_error_est(i))]);
         times(i) = toc;
         disp(['Completed test ', num2str(i), '/', num2str(par.numTests)]);
@@ -46,13 +70,17 @@ for j=1:length(testValues)
     coordination.mean_error_est(testValues_all == testValues(j), :) = mean_error_est;
     coordination.mean_error_trad(testValues_all == testValues(j), :) = mean_error_trad;
     coordination.mean_error_new(testValues_all == testValues(j), :) = mean_error_new;
+    coordination.max_error_prob(testValues_all == testValues(j),:) = max_error_prob;
+    coordination.max_error_est(testValues_all == testValues(j), :) = max_error_est;
+    coordination.max_error_trad(testValues_all == testValues(j), :) = max_error_trad;
+    coordination.max_error_new(testValues_all == testValues(j), :) = max_error_new;
     disp(['Completed test ', num2str(j), ' for ', num2str(testValues(j)), ' machines in ', num2str(mean(times)), ' seconds.']);
 end
 save(fullfile(par.scratchDir, ['coordination', num2str(pid)]), 'coordination');
 end
 
 function par = setup
-    par.scratchDir = 'K:\My Drive\School\Thesis\Synthetic_Dat_Gen\Data\scratch\tests';
+    par.scratchDir = 'C:\Users\cbonn\Documents\Thesis\Synthetic_Dat_Gen\Data\scratch\tests\tests';
 end
 
 % stationary distributions
